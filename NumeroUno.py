bb1384edc04c994b6f198899fcf3d1dfa7348070
BigBusinessMonkey <left_hand_man@outlook.com>
2016-07-12 23:08:22 +0100

    Add files via upload

---
+++ NumeroUno.py
@@ -0,0 +1,21 @@
+print("Welcome. This is the first program, please remember to correctly capitalise when answering the following questions.")
+userName = input("Please enter your name: ")
+userLang = input("What is your preferred interface language?")
+
+if(userLang == "English"):
+	greeting = "Hello "
+elif(userLang == "French"):
+	greeting = "Bonjour "
+elif(userLang == "German"):
+	greeting = "Guten Tag "
+else:
+	greeting = "Welcome "
+
+	
+if(userName == "Samuel" or userName == "Sam"):
+	print("Welcome, Master!")
+else:
+	print(greeting + userName + ".")
+
+if(greeting == "Welcome "):
+	print("Sorry, but your chosen language was not recognised.")
