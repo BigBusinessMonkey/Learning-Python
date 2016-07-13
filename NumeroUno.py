231a3c4c1bb4e5d5d63c66cf13639ced36282332
BigBusinessMonkey <left_hand_man@outlook.com>
2016-07-13 15:16:10 +0100

    Add files via upload

--- NumeroUno.py
+++ NumeroUno.py
@@ -1,21 +1,24 @@
-print("Welcome. This is the first program, please remember to correctly capitalise when answering the following questions.")
-userName = input("Please enter your name: ")
-userLang = input("What is your preferred interface language?")
+def userNameLang():
+	print("Welcome. This is the first program, please remember to correctly capitalise when answering the following questions.")
+	userName = input("Please enter your name: ")
+	userLang = input("What is your preferred interface language?")
 
-if(userLang == "English"):
-	greeting = "Hello "
-elif(userLang == "French"):
-	greeting = "Bonjour "
-elif(userLang == "German"):
-	greeting = "Guten Tag "
-else:
-	greeting = "Welcome "
+	if(userLang == "English"):
+		greeting = "Hello "
+	elif(userLang == "French"):
+		greeting = "Bonjour "
+	elif(userLang == "German"):
+		greeting = "Guten Tag "
+	else:
+		greeting = "Welcome "
 
-	
-if(userName == "Samuel" or userName == "Sam"):
-	print("Welcome, Master!")
-else:
-	print(greeting + userName + ".")
+		
+	if(userName == "Samuel" or userName == "Sam"):
+		print("Welcome, Master!")
+	else:
+		print(greeting + userName + ".")
 
-if(greeting == "Welcome "):
-	print("Sorry, but your chosen language was not recognised.")
+	if(greeting == "Welcome " and (userName != "Samuel" or userName != "Sam")):
+		print("Sorry, but your chosen language was not recognised.")
+		
+userNameLang()
